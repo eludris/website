c21a8bab72923e72c0bc75e6aeb55455425bfc52
Enoki <enokiun@gmail.com>
2022-08-29 23:13:45 +0300

    initial: Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# elusite
+
+A simple font page and blog website for eludris.
+
+Currently WIP
---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Eludris';
+export const SITE_DESCRIPTION = 'The official Eludris website.';
+export const EMBED_COLOUR = '#bc13fe';
